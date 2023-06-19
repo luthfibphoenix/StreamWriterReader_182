127bd893917ee652e750f69bcb64df93e12a0af7
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-19 14:47:42 +0700

    6

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -15,6 +15,10 @@ int main() {
 
 	//unlimited loop untuk menulis
 	while (true) {
-
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan huruf q
+		if (baris == "q") break;
 	}
 }
