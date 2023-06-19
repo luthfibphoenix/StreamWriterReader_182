79d287c9bb596ef53b2bcfa4cf8ce5192fea1950
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-19 15:09:33 +0700

    17

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -2,3 +2,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+
+	//membuka file dalam bentuk menulis.
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+}
