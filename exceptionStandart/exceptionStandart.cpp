87d46bdd84762991b795b518645683d5486fa83e
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-19 15:40:48 +0700

    26

--- exceptionStandart/exceptionStandart.cpp
+++ exceptionStandart/exceptionStandart.cpp
@@ -4,3 +4,19 @@
 #include <array>
 //untuk obyek yang akan kita gunakan
 using namespace std;
+
+int main()
+{
+	cout << "Awal Program" << endl; //penanda 1:..
+	try {
+		array<int, 3> data = { 1,2,3 };
+		//pesan array integer 3 elemen
+		cout << data.at(4) << endl;
+		//memanggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karena array data hanya memiliki 3 element*/
+	}
+}
